96ba7680d69e55bd48fcaf511521a2baf599d010
GenDave <GenDave@users.noreply.github.com>
2016-05-25 12:50:53 +0100

    Initial commit with nana_tv script added.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # nanaTV
-raspberry pi looping media player with 4 buttons to control the next video. Designed for old people.
+Raspberry pi looping media player with 4 buttons to control the next video. Designed for old people.
+
+This script run at boot will start playing random video's from a folder on the pi.
+It is designed for my grandmother who has altzimers.
+The buttons on the top give a choice of two specific video's, a folder of favourits and all video's.
+If any of the buttons are pressed, the current video is canceled and a new video is selected based on the button pressed.
+If one of the specified video buttons is pressed, the playlist returnes to random play when the video is finished. 
+If the favourites button is selected it will lock the playlist to the favourites list.
+If random is selected a new random video is selected and the playlist will be set to random.
+More button options can be created with ease and the program can be adapted to create a photo album.
---
+++ nana_tv.py
@@ -0,0 +1,81 @@
+## Nana tv v 2.0
+#
+# Button config
+# 1 Pride and predudice
+# 2 Bride and predudice
+# 3 Andre Rieu
+# 4 random
+
+import os
+import random
+import subprocess
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(4,GPIO.IN)
+GPIO.setup(17,GPIO.IN)
+GPIO.setup(27,GPIO.IN)
+GPIO.setup(22,GPIO.IN)
+
+# 10 second start to exit nanatv
+print("starting nanaTV in 10 seconds")
+time.sleep(10)
+print("starting nanaRV")
+
+# Generate file list
+
+mediaDir = '/home/pi/media/'
+fileList = []
+for root, dirnames, filenames in os.walk(mediaDir):
+  for filename in filenames:
+    fileList.append(os.path.join(root, filename))
+
+andreDir = '/home/pi/media/andre/'
+andreList = []
+for root, dirnames, filenames in os.walk(andreDir):
+  for filename in filenames:
+    andreList.append(os.path.join(root, filename))
+
+PrideFilm = '/home/pi/media/Pride_and_Prejudice.avi'
+BrideFilm = '/home/pi/media/Bride_and_Prejudice.avi'
+
+def getpin():
+    # get gpio pin and return
+    if (GPIO.input(4)):
+        pinno = 1
+    elif (GPIO.input(17)):
+        pinno = 2
+    elif (GPIO.input(27)):
+        pinno = 3
+    elif (GPIO.input(22)):
+        pinno = 4
+    else:
+        pinno = 0
+    return pinno
+    
+def playdata(fileList,playtype):
+    selectedFile = random.choice(fileList)
+    p = subprocess.Popen(["omxplayer","-b","-o" "hdmi",selectedFile])
+    while p.poll() is None:
+        pinno = getpin()
+        if pinno != 0: 
+            p.terminate()
+            playtype = pinno
+            break
+        time.sleep(0.5)
+    return playtype
+    
+
+playtype = 1
+while True:
+    if playtype == 1:
+        playdata(fileList,1)
+    elif playtype == 2:        
+        playdata(andreDir,2)
+    elif playtype == 3: 
+        playdata(PrideFilm,1)
+    elif playtype == 4:            
+        playdata(BrideFilm,1)     
+    else:
+        break
+    

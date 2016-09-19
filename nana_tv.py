214db2581cb7ee4573e191aff91844d84914b5ba
GenDave <GenDave@users.noreply.github.com>
2016-09-19 11:52:21 +0100

    Add files via upload
    Fixed multiple bugs including multiple instances of omxplayer.
    uploaded init script

--- nana_tv.py
+++ nana_tv.py
@@ -8,9 +8,13 @@
 
 import os
 import random
+# import signal
 import subprocess
 import time
 import RPi.GPIO as GPIO
+# Switch to below for testing on pc 
+# https://roderickvella.wordpress.com/2016/06/28/raspberry-pi-gpio-emulator/
+#from EmulatorGUI import GPIO 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4,GPIO.IN)
 GPIO.setup(17,GPIO.IN)
@@ -19,11 +23,11 @@ GPIO.setup(22,GPIO.IN)
 
 # 10 second start to exit nanatv
 print("starting nanaTV in 10 seconds")
-time.sleep(10)
+time.sleep(1)
 print("starting nanaRV")
 
 # Generate file list
-extensionList = [".avi"] # list of allowed media file extensions.
+extensionList = [".avi", ".mp4"] # list of allowed media file extensions.
 mediaDir = '/home/pi/media/'
 fileList = []
 for root, dirnames, filenames in os.walk(mediaDir):
@@ -37,9 +41,9 @@ for root, dirnames, filenames in os.walk(andreDir):
   for filename in filenames:
     if any([filename.endswith(ext) for ext in extensionList]):
       andreList.append(os.path.join(root, filename))
-
-PrideFilm = '/home/pi/media/Pride_and_Prejudice.avi'
-BrideFilm = '/home/pi/media/Bride_and_Prejudice.avi'
+# Duplicate pride and bridge films, so there is a list of more than one. otherwise ranom picks characters.
+PrideFilm = ['/home/pi/media/Pride_and_Prejudice.avi','/home/pi/media/Pride_and_Prejudice.avi']
+BrideFilm = ['/home/pi/media/Bride_and_Prejudice.avi','/home/pi/media/Bride_and_Prejudice.avi']
 
 def getpin():
     # get gpio pin and return
@@ -57,12 +61,18 @@ def getpin():
     
 def playdata(fileList,playtype):
     selectedFile = random.choice(fileList)
-    p = subprocess.Popen(["omxplayer","-b","-o" "hdmi",selectedFile])
+    p = subprocess.Popen(["omxplayer","-b","-o" "hdmi",selectedFile] ,stdin=subprocess.PIPE)
+    # Switch to below for testing on pc
+    #p = subprocess.Popen(["mplayer",selectedFile] -vop -zoom -x 1080 -y 1920 -fs)
     while p.poll() is None:
         pinno = getpin()
         if pinno != 0: 
-            p.terminate()
+            #p.kill()
+            os.system('killall omxplayer.bin')
+	    #os.killpg(os.getpgid(p.pid), signal,SIGTERM)
             playtype = pinno
+            while pinno != 0:
+                pinno = getpin()
             break
         time.sleep(0.5)
     return playtype
@@ -71,13 +81,13 @@ def playdata(fileList,playtype):
 playtype = 1
 while True:
     if playtype == 1:
-        playdata(fileList,1)
+        playtype = playdata(fileList,1)
     elif playtype == 2:        
-        playdata(andreDir,2)
+        playtype = playdata(andreList,2)
     elif playtype == 3: 
-        playdata(PrideFilm,1)
+        playtype = playdata(PrideFilm,1)
     elif playtype == 4:            
-        playdata(BrideFilm,1)     
+        playtype = playdata(BrideFilm,1)     
     else:
         break
     
